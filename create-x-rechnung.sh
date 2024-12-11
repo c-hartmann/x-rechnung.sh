9681a153cc8760f869b4e516dbdf7c6fa55d2564
Christian Hartmann <hartmann.christian@gmail.com>
2024-12-11 15:33:53 +0100

    add rc file definitions

--- create-x-rechnung.sh
+++ create-x-rechnung.sh
@@ -2,32 +2,43 @@
 
 export LANG=C
 
+# seller and other mostly ixed data go into a rc file
+x_rechnung_rc="$HOME/.config/x-rechnung.d/x-rechnung.rc"
+
+# find stylesheets and samples by a profile name
 typeset -l profile=${1:-'MINIMUM_Rechnung'} # e.g. 'minimum_rechnung'
 
-# TODO with or without underscores?
+# always / often reused customer fixed data might go into a rc file
+customer_key="$2:-'sample_customer'"
+customer_rc="$HOME/.config/x-rechnung.d/${customer_key}.rc"
+
+
+# TODO my names with or without underscores?
+# TODO use a csv for multiple products?
 
 # into +rc...
 vat_percentage=19
 SellerTradeParty_Name="Christian Hartmann"
-PostalTradeAddress_CountryID="DE" # default in Beispiel, but used
+PostalTradeAddress_CountryID='DE' # default in Beispiel, but used
 InvoiceCurrencyCode='EUR' # not used in stylesheet (coz default)
 
 
-# from command line
+# from command line or customer rc
 BuyerTradeParty_Name="seacamper"
 TaxBasisTotalAmount="11.99"
 
 # calculated if not given on command line
-TaxTotalAmount="${GrandTotalAmount:-$(echo "scale=2; ${TaxBasisTotalAmount} * ${vat_percentage} / 100" | bc)}"
-GrandTotalAmount=${GrandTotalAmount:-$(echo "scale=2; ${TaxBasisTotalAmount} * ( ${vat_percentage} + 100 ) / 100" | bc)}
-DuePayableAmount="${GrandTotalAmount}"
+TaxTotalAmount="${TaxTotalAmount:-$(echo "scale=2; ${TaxBasisTotalAmount} * ${vat_percentage} / 100" | bc)}"
+# GrandTotalAmount=${GrandTotalAmount:-$(echo "scale=2; ${TaxBasisTotalAmount} * ( ${vat_percentage} + 100 ) / 100" | bc)}
+GrandTotalAmount="${GrandTotalAmount:-$(echo "scale=2; ${TaxBasisTotalAmount} + ${TaxTotalAmount}" | bc)}"
+DuePayableAmount="${GrandTotalAmount}" # TODO whats the diff to GrandTotalAmount?
 
 
 
 # check stylesheet
 test -f "$profile.xslt" || error_exit "stylesheet not found for profile: $profile" 1
 
-# template for string params
+# template for string params to add
 # 	--stringparam ''       "$" \
 
 xsltproc \
